ba330ba4676432b7bdaa681d7441d02fa32dbf2e
Orevaoghene Eguwe <orevaoghene.raphael@gmail.com>
2024-09-11 14:49:30 +0100

    first step of serializations and deserialization

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,26 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
+    """containing attributes and methods for other classes"""
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def save(self):
+        """This method updates time when called"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """This method returns a dictionary of all key/values of __dict__ of instances"""
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = self.__class__.__name__
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
+        return inst_dict
+    
+    def __str__(self):
+        """return a string representation of the object in this format:
+        [<class name>] (<self.id>) <self.__dict__>"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
