c97263c53c08e55f38fa04032ae07a580a8eca74
Orevaoghene Eguwe <orevaoghene.raphael@gmail.com>
2024-09-13 14:12:19 +0100

    dynamically create model

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,19 @@ from datetime import datetime
 
 class BaseModel:
     """model containing attriubutes and methods for other classes"""
-    def __init__(self):
+    def __init__(self, **kwargs):
         """initialise the attributes of the class BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """save the updated time when called"""
